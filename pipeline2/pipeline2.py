d759f98fc01eb57e71c28b4741e5363253324883
Pavel Sozonov <pavel.sozonov@gmail.com>
2024-07-31 04:20:45 +0300

    Update pipeline

--- pipeline2/pipeline2.py
+++ pipeline2/pipeline2.py
@@ -1,18 +1,26 @@
-from kfp import dsl
-from kfp.v2 import compiler
-from kfp.v2.dsl import Output, Dataset
+from kfp import dst, compiler, Client
+from kfp.dsl import Dataset, Output, OutputPath, InputPath
+
+import warnings
+
 
 # Define Container Component using custom Docker image
-@dsl.component(
-    base_image='your-docker-repo/hello-world-app:latest'
-)
+@dsl.container_component
 def generate_file(str_amount: int, output_path: Output[Dataset]):
-    # This component will use the Docker container to run app.py
-    # The Docker image should handle the file generation based on the provided arguments
-    pass
+    return dsl.ContainerSpec(
+        image='custom-kfp-component:latest',
+        command=['python', 'app.py'],
+        args=[
+            '--str_amount', str_amount,
+            '--output_path', output_path.path
+        ]
+    )
 
 # Define Python Component to print the file
-@dsl.component
+@dsl.component(base_image="python:3.12",
+    pip_index_urls=["https://internal-repo", "https://internal-repo/repository/repo-name/simple"],
+    packages_to_install=[]
+)
 def print_file(input_path: Dataset):
     with open(input_path.path, 'r') as f:
         for line in f:
@@ -23,12 +31,21 @@ def print_file(input_path: Dataset):
     name='Hello World Pipeline',
     description='A pipeline that generates and prints a hello world file.'
 )
-def hello_world_pipeline(str_amount: int):
+def hello_world_pipeline(str_amount: int) -> Dataset:
     generate_task = generate_file(str_amount=str_amount)
     print_task = print_file(input_path=generate_task.outputs['output_path'])
+    return generate_task.outputs['output_path'] # Pipeline can be used as a component
 
 # Compile the Pipeline
 compiler.Compiler().compile(
     pipeline_func=hello_world_pipeline,
-    package_path='hello_world_pipeline.json'
+    package_path='hello_world_pipeline_custom_image.yaml'
+)
+
+# Run the Pipeline
+warnings simplefilter(action='ignore', category=FutureWarning) # Ignore kfp version warning
+run = Client().create_run_from_pipeline_package(
+    'hello_world_pipeline_custom_image.yaml',
+    experiment_name="Default",
+    arguments={'str_amount': 4}
 )
